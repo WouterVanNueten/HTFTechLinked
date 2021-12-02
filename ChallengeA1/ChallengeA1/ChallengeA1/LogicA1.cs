3ee96e466dab12dfd6302eef482782a91dac6660
WouterVanNueten <s107229@ap.be>
2021-12-02 12:24:33 +0100

    ChallengeA1 Completed

--- ChallengeA1/ChallengeA1/ChallengeA1/LogicA1.cs
+++ ChallengeA1/ChallengeA1/ChallengeA1/LogicA1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
 
 namespace ChallengeA1
 {
@@ -12,10 +14,64 @@ namespace ChallengeA1
         {
             HttpClient client = Client.GetHttpClient();
 
-            var startUrl = "api/path/1/easy/Start";
-            var startResponse = await client.GetAsync(startUrl);
+            //var startUrl = "api/path/1/easy/Start";
+            //var startResponse = await client.GetAsync(startUrl);
 
-            Console.WriteLine(startResponse);
+            //Console.WriteLine(startResponse);
+
+            var sampleUrl = "api/path/1/easy/Sample";
+            var sampleResponse = await client.GetFromJsonAsync<List<int>>(sampleUrl);
+
+            var sampleAnswer = GetAnswer(sampleResponse);
+
+            var samplePostResponse = await client.PostAsJsonAsync<int>(sampleUrl, sampleAnswer);
+            var samplePostResponseValue = await samplePostResponse.Content.ReadAsStringAsync();
+
+            Console.WriteLine(samplePostResponseValue);
+        }
+
+        static private int GetAnswer(List<int> list)
+        {
+            int numberSumResult = 0;
+
+            foreach(var number in list)
+            {
+                numberSumResult += number;
+            }
+
+            Console.WriteLine("Sum of all numbers: " + numberSumResult);
+
+            int digitSumResult;
+
+            do
+            {
+                List<int> splittedNumber = new List<int>();
+                while (numberSumResult > 0)
+                {
+                    splittedNumber.Add(numberSumResult % 10);
+                    numberSumResult /= 10;
+                }
+                splittedNumber.Reverse();
+
+                Console.WriteLine("Seperated digits:");
+
+                digitSumResult = 0;
+
+                foreach (var digit in splittedNumber)
+                {      
+                    Console.Write(digit + " ");
+                    digitSumResult += digit;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Result of sum of digits: " + digitSumResult);
+                numberSumResult = digitSumResult;
+            }
+            while (digitSumResult > 9);
+
+            Console.WriteLine("One digit left");
+
+            return digitSumResult;
         }
     }
 }
